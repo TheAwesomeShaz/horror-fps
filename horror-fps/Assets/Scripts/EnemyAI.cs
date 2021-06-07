8593415d87d73962f8bf14663f870cd84e193e4a
Knightmare-sama <shahbaaz23khan@gmail.com>
2021-06-07 22:47:57 +0530

    initial commit

---
+++ horror-fps/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    [SerializeField] float chaseRange = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+}
